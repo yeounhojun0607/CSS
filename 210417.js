47cc2f4b79b410f084446d4372e866d70f096b22
yeounhojun0607 <70273540+yeounhojun0607@users.noreply.github.com>
2022-04-17 15:04:21 +0900

    Update 210417.js

--- 210417.js
+++ 210417.js
@@ -3,6 +3,6 @@ style.type = "text/css";
 var head = document.head;
 head.appendChild(style);
 
-style.sheet.insertRule("* { font-family: 'Zapfino', cursive !important; }");
+style.sheet.insertRule("* { font-family: 'NanumBarunpen Regular', cursive !important; }");
 
 completion(true);
