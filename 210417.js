698eb31ff6323d062be7c29990301c7d91f19919
yeounhojun0607 <70273540+yeounhojun0607@users.noreply.github.com>
2022-04-17 15:03:04 +0900

    Update and rename 210417.css to 210417.js

---
+++ 210417.js
@@ -0,0 +1,8 @@
+var style = document.createElement("style");
+style.type = "text/css";
+var head = document.head;
+head.appendChild(style);
+
+style.sheet.insertRule("* { font-family: 'NanumBarunpen', cursive !important; }");
+
+completion(true);
