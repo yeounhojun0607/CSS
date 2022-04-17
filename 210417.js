b0d84768d239572bd8eaa1be85b3c5fb88db2054
yeounhojun0607 <70273540+yeounhojun0607@users.noreply.github.com>
2022-04-17 15:09:19 +0900

    Update 210417.js

--- 210417.js
+++ 210417.js
@@ -3,6 +3,6 @@ style.type = "text/css";
 var head = document.head;
 head.appendChild(style);
 
-style.sheet.insertRule("* { font-family: 'Sandoll GogoRound', cursive !important; }");
+style.sheet.insertRule("* { font-family: 'Sandoll GogoRound 03 Rg', cursive !important; }");
 
 completion(true);
