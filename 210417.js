44d9f8ce672bfe485e96c8d1b30766b742375b2c
yeounhojun0607 <70273540+yeounhojun0607@users.noreply.github.com>
2022-04-18 15:14:34 +0900

    Update 210417.js

--- 210417.js
+++ 210417.js
@@ -3,6 +3,6 @@ style.type = "text/css";
 var head = document.head;
 head.appendChild(style);
 
-style.sheet.insertRule("* { font-family: 'NanumBarunpen', cursive !important; }");
+style.sheet.insertRule("* { font-family: '나눔바른펜', cursive !important; }");
 
 completion(true);
